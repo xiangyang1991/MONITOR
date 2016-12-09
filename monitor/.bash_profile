65e8573ec7f1bfdf6c03854afed12b3667aecf39
Xiangyang Li <xiangyang_git@163.com>
2016-12-09 19:21:53 +0800

    set ENV

---
+++ monitor/.bash_profile
@@ -0,0 +1,3 @@
+#!/bin/bash
+MONITOR_HOME="${HOME}/git/MONITOR/monitor"
+MONITOR_ENV="${MONITOR_HOME}/.MONITOR_ENV"
---
+++ monitor/check-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#MONITOR_HOME=$(cd `dirname $0`; pwd)
+#ENV="${MONITOR_HOME}/.ENV"
+#. ${MONITOR_HOME}/.ENV_FILE
+#echo $Load
+
+
+#The MONITOR_HOME was set in .bash_profile
+. ~/.bash_profile
+. ${MONITOR_ENV}/.ENV_FILE
