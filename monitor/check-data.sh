c9a4e110f46e19db56add7308258e69bee78d19a
Xiangyang Li <xiangyang_git@163.com>
2016-12-09 14:24:30 +0800

    ENV_FILE

--- monitor/check-data.sh
+++ monitor/check-data.sh
@@ -3,8 +3,9 @@
 #ENV="${MONITOR_HOME}/.ENV"
 #. ${MONITOR_HOME}/.ENV_FILE
 #echo $Load
-
-
 #The MONITOR_HOME was set in .bash_profile
 . ~/.bash_profile
 . ${MONITOR_ENV}/.ENV_FILE
+echo ${MONITOR_ENV}
+echo ${MONITOR_HOME}
+echo -e "${green_1}${Load}${end}"
